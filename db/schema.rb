4dd4de429458f56f95f20eecc82b287b80cc6020
Tamer Dogan <pelintamerdogan@gmail.com>
2020-11-08 12:24:33 +0100

    Added member id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_220400) do
+ActiveRecord::Schema.define(version: 2020_11_08_112012) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_220400) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "member_id"
   end
 
 end
