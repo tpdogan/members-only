81c0f940698d52cd147115e9e82377474a6fc930
Tamer Dogan <pelintamerdogan@gmail.com>
2020-11-08 20:34:42 +0100

    changed member icon to show when signed in

--- db/migrate/20201108190539_add_icon_to_members.rb
+++ db/migrate/20201108190539_add_icon_to_members.rb
@@ -1,6 +1,6 @@
 class AddIconToMembers < ActiveRecord::Migration[5.2]
   def change
     add_column :members, :icon, :string
-    change_column_default :members, :icon, "https://www.flaticon.com/premium-icon/icons/svg/2989/2989915.svg"
+    change_column_default :members, :icon, "https://www.flaticon.com/svg/static/icons/svg/1077/1077114.svg"
   end
 end
