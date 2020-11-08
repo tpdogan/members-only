a290bf69014d11e54eac6f256f2ebf89145cf46e
Tamer Dogan <pelintamerdogan@gmail.com>
2020-11-08 13:55:46 +0100

    Added errors to login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :members, :controllers => { registrations: "registrations" }
   resources :posts
   root to: "posts#index"
+  get '*path' => redirect('/')
 end
